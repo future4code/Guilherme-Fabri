18cf51c63d7275efbca1b0cccbfde6a0aa590c1d
GUILHERME SOUTELLO <guisoutello@gmail.com>
2021-03-01 22:21:39 -0300

    Exercicios-de-análise

---
+++ semana04/funcoes/funcoes.js
@@ -0,0 +1,14 @@
+//exercicio 1
+//a) Será impresso:
+// 10
+// 50
+//b) As funcões serão invocadas mas não serão 'printadas' no console.
+
+//exercicio 2
+//a) Será impresso: Darvas e Caio.
+//b) Se arrayDeNomes mudar o valor impresso será: Amanda e Caio
+
+//exercicio 3
+
+//exercicio 4
+
